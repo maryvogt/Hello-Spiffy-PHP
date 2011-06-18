61fbde8ac15bc7df03e9d3fa01616dab03b70a2f
Mary Vogt <mary.vogt@comcast.net>
2011-06-17 21:34:09 -0400

    Send back some JSON from PHP, including the username from the URL parameter

--- hellospiffyphp.php
+++ hellospiffyphp.php
@@ -1,4 +1,12 @@
 <?php
-echo 'Hello ' . htmlspecialchars($_GET["name"]) . '!';
+//echo 'Hello ' . htmlspecialchars($_GET["name"]) . '!';
+
+
 ?>
 
+
+{
+    "user": <?php echo(json_encode(htmlspecialchars($_GET["name"]))) ?>,
+    "userAgent": "Firefox",
+    "serverInfo": "Zack's frog"
+}
