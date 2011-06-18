a860441cf338b3d1f5d4724a5a429796c42a13f1
Mary Vogt <mary.vogt@comcast.net>
2011-06-18 16:28:13 -0400

    made functions, added info about PHP version and server software.

--- hellospiffyphp.php
+++ hellospiffyphp.php
@@ -1,7 +1,52 @@
+<?php
 
-{
-"user": <?php echo(json_encode(htmlspecialchars($_GET["name"]))); ?>,
-"userAgent": <?php echo(json_encode(htmlspecialchars($_SERVER['HTTP_USER_AGENT']))) ?>,
-"serverInfo": <?php echo (json_encode(htmlspecialchars($SERVER['SERVER_NAME']))) ?>
+function get_user_encoded(){
 
+	return json_encode(htmlspecialchars($_GET["name"]));
 }
+
+function get_agent_encoded(){
+
+	if(isset($_SERVER['HTTP_USER_AGENT'])){
+		// no need for htmlspecialchars, userAgent won't have them
+   		return json_encode($_SERVER['HTTP_USER_AGENT']);
+   	}
+}
+
+function emit_server_info(){
+
+    $str = gethostname();
+    $str = $str . " (PHP: " . phpversion();
+    
+    
+    if(isset($_SERVER['SERVER_SOFTWARE'])){
+        $str = $str . ", server software: " . $_SERVER['SERVER_SOFTWARE'] ;
+    }
+    
+    $str = $str . ")";
+   	
+  	echo json_encode($str);
+}
+
+function emit_json_data(){
+
+    header("Content-Type: application/json");
+    ?> 
+    
+    {
+    "debugversion" : "406",
+    "user" : <?php echo(get_user_encoded()) ?> ,
+    "userAgent" : <?php echo(get_agent_encoded()) ?>,
+    "serverInfo" : <?php emit_server_info() ?>    
+    }
+    <?php
+}
+
+emit_json_data();
+?> 
+
+
+
+
+
+
